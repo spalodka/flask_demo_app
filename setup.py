e51d10370edf881e480b5d7b7ec73581d92b2d51
spalodka <sandippalodkar7@gmail.com>
2021-08-20 07:37:35 -0700

    updated setup file with syntax correction

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.MD",r) as file:
 		long_description = file.read()
 
 setuptools.setup(
-	name='preprocess_iampossible' #this should be unique 
-	version = '0.0.1'
+	name='preprocess_iampossible', #this should be unique 
+	version = '0.0.1',
 	author = 'Sandip Palodkar'
-	#author_email ='sandippalodkar7@gmail.com'
+	#author_email ='sandippalodkar7@gmail.com',
 	description = 'This is preprocessing package',
 	long_description = long_description,
 	long_description_content_type = 'text/markdown',
